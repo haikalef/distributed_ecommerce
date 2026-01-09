68c71a18173c95d38078213acb905f64bf394663
haikalef <92344091+haikalef@users.noreply.github.com>
2026-01-09 19:21:21 +0700

    Fix database.py: change MySQL host from localhost to mysql (Docker service name)

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://app_user:app_password@localhost/distributed_ecommerce"
-
+DATABASE_URL = "mysql+pymysql://app_user:app_password@mysql/distributed_ecommerce"
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
